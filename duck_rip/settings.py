5b5dfff347b6dbf58388cc279f6c0bef89c9788d
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-08-05 22:45:09 +0200

    API welcome page powered by bootstrap

--- duck_rip/settings.py
+++ duck_rip/settings.py
@@ -1,10 +1,14 @@
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__ + '/../../'))
+
 # Django settings for duck_rip project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Tomasz Ducin', 'tomasz.ducin [at] gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -31,7 +35,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Warsaw'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -63,17 +67,15 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, "assets")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, "static/bootstrap"),
 )
 
 # List of finder classes that know how to find static files in
